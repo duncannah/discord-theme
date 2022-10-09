50c327d27f67a697e3d9e993760e2feca10010ec
duncannah <38371914+duncannah@users.noreply.github.com>
2022-10-09 13:56:02 +0200

    feat: always fetch css if last fetch was >24h

--- src/overwrite/fetch-css.js
+++ src/overwrite/fetch-css.js
@@ -2,21 +2,29 @@ const fs = require("node:fs");
 const path = require("node:path");
 const prettier = require("prettier");
 
-module.exports = async function () {
-	if (!fs.existsSync(path.join("discord.css"))) {
-		console.log("Downloading discord.css...");
-		await fetch("https://discord.com/app")
-			.then((res) => res.text())
-			.then(async (body) => {
-				let match = body.match(/<link rel="stylesheet" href="(.*?)"/i);
+async function fetch() {
+	console.log("Downloading discord.css...");
+	await fetch("https://discord.com/app")
+		.then((res) => res.text())
+		.then(async (body) => {
+			let match = body.match(/<link rel="stylesheet" href="(.*?)"/i);
 
-				if (match) {
-					fs.writeFileSync(
-						path.join("discord.css"),
-						await fetch("https://discord.com/" + match[1]).then((res) => res.text())
-					);
-				} else throw new Error("Could not find discord.css");
-			});
+			if (match) {
+				fs.writeFileSync(
+					path.join("discord.css"),
+					await fetch("https://discord.com/" + match[1]).then((res) => res.text())
+				);
+			} else throw new Error("Could not find discord.css");
+		});
+}
+
+module.exports = async function () {
+	if (!fs.existsSync(path.join("discord.css"))) await fetch();
+	else {
+		const stats = fs.statSync(path.join("discord.css"));
+		const diff = new Date().getTime() - stats.mtime.getTime();
+
+		if (diff > 1000 * 60 * 60 * 24) await fetch();
 	}
 
 	if (
